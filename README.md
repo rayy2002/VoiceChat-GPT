197ecf0a2d882a1924474df5283754049fd6e328
rayy2002 <raihansoni2002@gmail.com>
2023-07-27 16:55:53 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Voice ChatGPT
+
+Creating the "VoiceChat GPT" project involved leveraging the combined power of Bluetooth communication, Python programming, and Raspberry Pi hardware to build a sophisticated and conservative conversational AI device with voice interaction and speaker output.
+
+# Hardware Setup
+
+Raspberry Pi: A Raspberry Pi serves as the brain of the "VoiceChat GPT" device. It runs the Python-based AI application, handles user input, processes the AI responses, and manages the Bluetooth communication with the monospeaker.
+
+Monospeaker: The monospeaker, paired with the Raspberry Pi via Bluetooth, functions as both the audio input and output device. Users interact with the "VoiceChat GPT" device by speaking their questions to the monospeaker, which captures the audio and sends it to the Raspberry Pi for processing.
+
+## Work Flow
+First User inut is captured via BT monospeaker
+
+After capturing the user's voice input and converting it to text, the text is sent for processing using an unofficial API to interact with ChatGPT. This custom API allows the "VoiceChat GPT" device to leverage the power of ChatGPT's language model capabilities. The unofficial API takes the user's text query as input and sends it to ChatGPT, where the language model processes the text and generates a response.
+
+Once ChatGPT's response is obtained as a text string from the unofficial API, the "VoiceChat GPT" device proceeds to convert this text response into audible speech using a Text-to-Speech (TTS) engine. The TTS-generated speech is then transmitted via Bluetooth to the monospeaker, where it is played back for the user to hear.
+
+## Chat GPT API
+
+https://github.com/acheong08/ChatGPT-lite.git
+
+
+## Demo
+
+Insert gif or link to demo
+
---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import openai
+from os.path import join,dirname
+from dotenv import load_dotenv
+import time
+import speech_recognition as sr
+import pyttsx3
+import numpy as np
+# from os.path import join, dirname
+# import matplotlib.pyplot as plt
+# ^ matplotlib is great for visualising data and for testing purposes but usually not needed for production
+
+#load_dotenv()
+openai.api_key = "pk-LyrweYxSaaVUgpNnYQzvOBmhBMFotDDlGeduvBBLGRvvkDvY"
+openai.api_base = 'https://api.pawan.krd/v1'
+model = 'gpt-3.5-turbo'
+# Set up the speech recognition and text-to-speech engines
+r = sr.Recognizer()
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')
+engine.setProperty('voice' , 'english-us')
+#engine.setProperty('voice', voice.id)
+name = "YOUR NAME HERE"
+greetings = [f"whats up master {name}", 
+             "yeah?", 
+             "Well, hello there, Master of Puns and Jokes - how's it going today?",
+             f"Ahoy there, Captain {name}! How's the ship sailing?",
+             f"Bonjour, Monsieur {name}! Comment ça va? Wait, why the hell am I speaking French?" ]
+
+# Listen for the wake word "hey pos"
+def listen_for_wake_word(source):
+    print("Listening...")
+    engine.say("Listening")
+    engine.runAndWait()
+
+    while True:
+        audio = r.listen(source)
+        try:
+            print('SAY')
+            text = r.recognize_google(audio)
+            print(text)
+            if "wake up GPT" or "I have another question" in text.lower():
+                print("Wake word detected.")
+                engine.say("Yes master")
+                engine.runAndWait()
+                listen_and_respond(source)
+                break
+        except sr.UnknownValueError:
+            pass
+
+# Listen for input and respond with OpenAI API
+def listen_and_respond(source):
+    print("Listening...")
+
+    while True:
+        audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print(f"You said: {text}")
+            if not text:
+                continue
+
+            # Send input to OpenAI API
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}]) 
+            response_text = response.choices[0].message.content
+            print(f"OpenAI response: {response_text}")
+
+            # Speak the response
+            engine.say(response_text)
+            engine.runAndWait()
+            time.sleep(3)
+
+            if not audio:
+                listen_for_wake_word(source)
+        except sr.UnknownValueError:
+            time.sleep(5)
+            print("Silence found, shutting up, listening...")
+            listen_for_wake_word(source)
+            break
+            
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+            engine.say(f"Could not request results; {e}")
+            engine.runAndWait()
+            listen_for_wake_word(source)
+            break
+
+# Use the default microphone as the audio source
+with sr.Microphone() as source:
+    listen_for_wake_word(source)
